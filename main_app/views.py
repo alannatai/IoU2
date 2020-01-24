48767327a83b95d4bb4214c696fd0144e0c7c1e7
louis-pham <l.c.pham@hotmail.com>
2020-01-24 17:44:27 -0500

    - Add templates folder - create login and homepage views/url

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>IoU2</h1>')
+    return render(request, "index.html")
