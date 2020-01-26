5e28c2064b4e0e4f05c600284328469726f74b24
Alanna Tai <alanna.tai@hotmail.com>
2020-01-25 23:39:13 -0500

    Able to log in and sign up. Also added a template for the household index.

--- main_app/models.py
+++ main_app/models.py
@@ -1,24 +1,32 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-#Household model
 class Household(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"Household: {self.name}"
 
-#Users
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     household = models.ManyToManyField(Household)
     # budgetb
 
     def __str__(self):
-        return f"Member: {self.name}""
+        return f"Member: {self.user}"
 
-#Expenses
+# 'signals' so our Member model will auto create/update when User is created/updated
+@receiver(post_save, sender=User)
+def create_user_member(sender, instance, created, **kwargs):
+    if created:
+        Member.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_member(sender, instance, **kwargs):
+    instance.member.save()
+
 class Expense(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -30,7 +38,6 @@ class Expense(models.Model):
     def __str__(self):
         return f"{self.who_paid} bought {self.name} for {self.cost}"
 
-#Expense Split
 class Split(models.Model):
     amount_owed = models.FloatField()
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('households/', views.households_index, name='households_index'),
+
+  path('accounts/signup/', views.signup, name='signup')
 ]
+
