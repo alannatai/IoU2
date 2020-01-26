b64941294a711f7356dce791cc88bb960103b42f
Alanna Tai <alanna.tai@hotmail.com>
2020-01-25 23:48:28 -0500

    Added about route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
   path('households/', views.households_index, name='households_index'),
 
   path('accounts/signup/', views.signup, name='signup')
