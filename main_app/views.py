72adddba963e46180e206e1274c8c05f6ab641aa
louis-pham <l.c.pham@hotmail.com>
2020-01-27 13:09:15 -0500

    Add houshold update view/templates (still need to be able to add members)

--- main_app/views.py
+++ main_app/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Household
 
 def home(request):
     return render(request, 'index.html')
-    
+
 def about(request):
     return render(request, 'about.html')
 
@@ -48,11 +48,11 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
-def delete_household(request):
-    households = Household.objects.filter()
-    return render(request, households/index.html",{
-        
-    })
+# def delete_household(request):
+#     households = Household.objects.filter()
+#     return render(request, households/index.html",{
+#
+#     })
 
 class HouseholdCreate(LoginRequiredMixin, CreateView):
     model = Household
@@ -62,3 +62,7 @@ class HouseholdCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class HouseholdUpdate(LoginRequiredMixin, UpdateView):
+    model = Household
+    fields = ["name"]
