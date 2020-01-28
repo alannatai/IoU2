a746c8c8dd555954dc8e0e0f1cc5fa4891a83182
louis-pham <l.c.pham@hotmail.com>
2020-01-28 12:40:46 -0500

    - changed member from one2one with user model to extends AbstractUser - renamed m2m fields for household and member - refactored member fields in expense and split model - refactored views for model changes - refactored HouseholdForm for model changes - refactored household_update template for model changes - refactored household_details template for model changes - created new migrations - updated settings to point to Member model instead of default User

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from .models import Household, Member, Expense, Split
 
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = Member
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('households',)}),
+    )
 
 # Register your models here.
+admin.site.register(Member, MyUserAdmin)
 admin.site.register(Household)
-admin.site.register(Member)
 admin.site.register(Expense)
 admin.site.register(Split)
